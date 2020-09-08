46f0ab11316993a3e49db4d0653717d6ef9912e5
Renzo Olivares <renzo.olivares1@gmail.com>
2020-09-07 21:49:50 -0700

    Fix going back when on compose

--- lib/mail_view_router.dart
+++ lib/mail_view_router.dart
@@ -68,6 +68,7 @@ class MailViewRouterDelegate extends RouterDelegate<void>
 
     //Handles the back button when on the ComposePage.
     if (onCompose) {
+      emailStore.onCompose = false;
       return SynchronousFuture<bool>(false);
     }
 
@@ -83,6 +84,7 @@ class MailViewRouterDelegate extends RouterDelegate<void>
     // Handles the back button press when on the MailView. If there is a route
     // to pop then pop it, and reset the currentlySelectedEmailId to -1
     // to notify listeners that we are no longer on the MailView.
+    print('over here');
     if (navigatorKey.currentState.canPop()) {
       navigatorKey.currentState.pop();
       Provider.of<EmailStore>(navigatorKey.currentContext, listen: false)
